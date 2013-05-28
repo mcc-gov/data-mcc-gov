571ac83dae124be87b7415636b0b5ddb21e46c22
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2013-05-28 13:03:13 -0400

    refactored API to serve all csv datasets from data.json as JSON

--- web/main.py
+++ web/main.py
@@ -1,11 +1,13 @@
 import json
 from flask import Flask
 from flask import render_template
+import requests
+import StringIO
 
 app = Flask(__name__)
 
 #location of data.json folder on server e.g. /var/www/smthing
-root=""
+root="/var/www/raw/"
 
 @app.route('/')
 def hello_world():
@@ -54,6 +56,59 @@ def hello(identifier=None):
 			break
 	return render_template('meta.html', meta=meta)
 
+@app.route('/api')
+def api(identifier=None):
+	return """{"url": "http://data.mcc.gov/api", "resources":{"list":{"url":http://data.mcc.gov/api/list"}}"""
+
+
+@app.route('/api/list')
+def api_list(identifier=None):
+	f=open(root+"data.json")
+	data = json.loads(f.read())
+	f.close()
+
+	items={}
+	for item in data:
+		try:
+			for distr in item["distribution"]:
+				if distr["format"]=="csv":
+					items[item["identifier"]]={"url":"http://data.mcc.gov/api/dataset/"+item["identifier"]}
+		except:
+			pass
+
+	return json.dumps(items, indent=4)
+
+import csv
+
+@app.route('/api/dataset/<identifier>')
+def api_dataset(identifier=None):
+	f=open(root+"data.json")
+	data = json.loads(f.read())
+	f.close()
+
+	url=""
+	for item in data:
+		if identifier==item["identifier"]:
+			for distr in item["distribution"]:
+				if distr["format"]=="csv":
+					url=distr["accessURL"]
+	
+	data = requests.get(url).text
+	csv_reader = csv.reader( StringIO.StringIO(data) )
+	header=[]
+	dataset=[]
+	for i,row in enumerate(csv_reader):
+		if i==0:
+			header=row
+		else:
+			el={}
+			for j, item in enumerate(row):
+				el[header[j]]=item
+			dataset.append(el)
+
+	return json.dumps(dataset, indent=4)
+
+
 if __name__ == '__main__':
-	app.debug = True
+	#app.debug = True
 	app.run()
